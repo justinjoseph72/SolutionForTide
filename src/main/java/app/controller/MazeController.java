5a8566de8ce0b18b98ddda7b8161d2eb63c97573
Justin Joseph <jjo@commsoft.co.uk>
2017-05-20 15:54:02 +0100

    Tide updating the input maze to string and then converting it to char array

--- src/main/java/app/controller/MazeController.java
+++ src/main/java/app/controller/MazeController.java
@@ -4,15 +4,15 @@ import app.labEscape.LabEscape;
 import app.labEscape.NoEscapeException;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.StringTokenizer;
 
 /**
  * Created by Justin on 20/05/2017.
@@ -22,26 +22,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MazeController {
 
+    private char[][] initalArray;
+
     /**
      * This method will map the path "/findMaze" to a rest full API
      * @param maze  will be input char array
      * @param startX will be starting x position
      * @param startY will be starting y poistion
      * @return ResponseEnity with following values
-     *          OK if a path is dfound
+     *          OK if a path is found
      *          Not Found if there is a NoEscapeException thrown
-     * I        nternal Server Error for any other exceptions
+     *          Internal Server Error for any other exceptions
      */
 
     @ApiOperation(value = "This method will be used to find escape route from maze")
     @RequestMapping(value="/findExit", method = RequestMethod.GET)
     public ResponseEntity<String> getMazeSolution(
-            @ApiParam(value = "this is the input maze") @RequestParam char[][] maze,
+            @ApiParam(value = "this is the input maze") @RequestParam String maze,
             @ApiParam(value = "this is the start X value") @RequestParam int startX,
             @ApiParam(value = "this is the start Y value") @RequestParam int startY )
     {
         try{
-            char [][] output = LabEscape.drawPathForEscape(maze,startX,startY);
+            initalArray = tokeize(maze);
+            char [][] output = LabEscape.drawPathForEscape(initalArray,startX,startY);
             HttpHeaders headers = new HttpHeaders();
             headers.set("result",output.toString());
             return new ResponseEntity<String>(HttpStatus.OK);
@@ -53,5 +56,28 @@ public class MazeController {
             return  new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    private char[][] tokeize(String baseString){
+        StringTokenizer tokenizer = new StringTokenizer(baseString,"|");
+        char[][] initialArray = new char[tokenizer.countTokens()][];
+        int rowCOunter = 0;
+        while (tokenizer.hasMoreTokens()){
+            String arr = tokenizer.nextToken();
+            StringTokenizer innerTokenizer = new StringTokenizer(arr.substring(1,arr.length()),",");
+            char[] innerArray = new char[innerTokenizer.countTokens()];
+            int columnCounter = 0;
+            while (innerTokenizer.hasMoreTokens()){
+                String add = innerTokenizer.nextToken();
+               innerArray[columnCounter] = add.charAt(0);
+                columnCounter ++;
+            }
+            initialArray[rowCOunter] = innerArray;
+            rowCOunter ++;
+        }
+
+        return initialArray;
+
+    }
+
 }
 
